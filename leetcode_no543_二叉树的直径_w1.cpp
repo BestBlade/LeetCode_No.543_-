fd22b340dc35ab1002868d9e378c929fc4fb7e65
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-03-29 17:51:30 +0800

    Add files via upload

---
+++ leetcode_no543_二叉树的直径_w1.cpp
@@ -0,0 +1,86 @@
+﻿/* ------------------------------------------------------------------|
+给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
+
+ 
+
+示例 :
+给定二叉树
+
+          1
+         / \
+        2   3
+       / \
+      4   5
+返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+
+ 
+
+注意：两结点之间的路径长度是以它们之间边的数目表示。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/diameter-of-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	dfs递归法
+*
+*	执行用时：4 ms, 在所有 C++ 提交中击败了98.87%的用户
+*	内存消耗：19.8 MB, 在所有 C++ 提交中击败了48.04%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+int maxval = 0;
+
+int dfs(TreeNode* root) {
+    if (!root) {
+        return 0;
+    }
+    int left = dfs(root->left);
+    int right = dfs(root->right);
+    maxval = max(maxval, left + right);
+    return max(left, right) + 1;
+}
+
+int diameterOfBinaryTree(TreeNode* root) {
+    dfs(root);
+    return maxval;
+}
